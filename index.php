78aacd832a114be43f0466d5435ed8442c5043f9
Husuch <nicolas.hausr@gmail.com>
2019-11-12 15:52:46 +0100

    TimeLine

--- index.php
+++ index.php
@@ -1,10 +1,172 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <link rel="stylesheet" href="css/timeLine.css"/>
+    <link rel="stylesheet" href="css/index.css"/>
+    <title>Wirtschaftliche Entwicklung</title>
 </head>
 <body>
+<div class="gridBox">
 
+    <div class="topNav">
+        <a class="home" href="index.php"><i class="fas fa-home"></i> Home</a>
+    </div>
+    <div class="timeline">
+
+        <div class="container left">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container right">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container left">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container right">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container left">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container right">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container left">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+        <div class="container right">
+            <div class="content">
+                <h2>Jahr</h2>
+                <p>Ein Computerspiel ist ein Computerprogramm, das einem oder mehreren Benutzern ermöglicht, interaktiv
+                    ein durch implementierte Regeln beschriebenes Spiel zu spielen. Umgangssprachlich wird auch der
+                    Begriff Game verwendet (von englisch game ‚Spiel‘).
+
+                    Obwohl alle Spiele auf computerbasierten Geräten wie PC, Arcade-Automat, Smartphone und Spielkonsole
+                    Computerspiele sind, wird umgangssprachlich der Begriff Computerspiel vornehmlich für Spiele auf PCs
+                    verwendet. Der Begriff Videospiel (englisch Video game) bezeichnet alle elektronischen
+                    visuell-basierten Spiele und wird meist für PC-, Arcade-, Mobil- und Konsolenspiele verwendet.
+
+                    Das Forschungsgebiet Ludologie befasst sich mit Computerspielen auch in theoretischer Hinsicht,
+                    wobei die Spiele als Produkte und das Spielen als Handlung in Betracht gezogen werden.[1]</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="footer">
+
+        <h2>Quellen</h2><br>
+
+            <a href="index.html">Wikipedia</a>
+            ·
+            <a href="html/footer/about.html">Digitec</a>
+            ·
+            <a href="html/footer/contact.html">Galaxus </a>
+            ·
+            <a href="html/footer/followus.html">Migros</a>
+
+
+    </div>
+
+</div>
 </body>
 </html>
